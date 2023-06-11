91ebd555e4a32a5dfa3c2b08d2c63a176c5adbe9
Tamuda <tchimhan@u.rochester.edu>
2023-06-11 11:55:41 -0400

    testing landing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -115,7 +115,7 @@ function Newsletter() {
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
+        <span className="ml-3">Stay up to date on everything</span>
       </h2>
       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
         Get notified when I publish something new, and unsubscribe at any time.
